dd86fb2572a046a19325de7c1693c4155e6eae8f
Kiwa N <nhlanhlakiwa@gmail.com>
2025-07-17 16:56:46 +0200

    new server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { Collection } = require('mongodb')
-const e = require('express')
-const MongoClient =  require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+const PORT = 8000
 
 let db,
-    dbConnectionString = process.env.DB_SRING,
-    dbName = 'project_new',
-    collection 
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'sample_mflix',
+    collection
 
 MongoClient.connect(dbConnectionString)
     .then(client => {
@@ -18,12 +17,22 @@ MongoClient.connect(dbConnectionString)
         collection = db.collection('movies')
     })
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
 
-app.listen(process.env.PORT || PORT,()=>{
-    console.log(`server is running on port `)
+app.get('/', async (request, response) => {
+    try {
+        response.render('index.ejs')
+    } catch (error) {
+        response.status(500).send({message: error.message})
+    }
+})
+
+
+//PORT = 8000
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port`)
 })
